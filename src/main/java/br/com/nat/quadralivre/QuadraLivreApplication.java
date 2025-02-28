5187d62333375b3d1d6450cf61b38253c0c59082
nataliafrancisca <nathifrancisca@gmail.com>
2025-02-27 23:23:05 -0300

    feat: init repository

---
+++ src/main/java/br/com/nat/quadralivre/QuadraLivreApplication.java
@@ -0,0 +1,13 @@
+package br.com.nat.quadralivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadraLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadraLivreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nat/quadralivre/QuadraLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nat.quadralivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadraLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
