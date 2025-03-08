82fbfed0acf7c1ff8ec64a16ba864a4c51f84f5e
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-07 21:10:57 -0300

    feat: criei todos os arquivos relacionados a entidade Responsavel

---
+++ src/main/java/br/com/nat/quadralivre/dto/ResponsavelDTO.java
@@ -0,0 +1,43 @@
+package br.com.nat.quadralivre.dto;
+
+import br.com.nat.quadralivre.model.Responsavel;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponsavelDTO {
+    @NotBlank(message = "CPF é obrigatório.")
+    @CPF
+    private String cpf;
+
+    @NotBlank(message = "Nome é obrigatório.")
+    private String nome;
+
+    @NotBlank(message = "Número para contato é obrigatório.")
+    @Pattern(regexp = "^\\d{2}\\s9?\\d{4}-\\d{4}$", message = "Número de telefone inválido. Use o formato 'DD' XXXX-XXXX ou 'DD 9XXXX-XXXX'.")
+    private String telefone;
+
+    @NotBlank(message = "Endereço de e-mail é obrigatório.")
+    @Email
+    private String email;
+
+    public static Responsavel toEntity(ResponsavelDTO responsavelDTO){
+        Responsavel responsavel = new Responsavel();
+
+        responsavel.setCpf(responsavelDTO.getCpf());
+        responsavel.setNome(responsavelDTO.getNome());
+        responsavel.setTelefone(responsavelDTO.getTelefone());
+        responsavel.setEmail(responsavelDTO.getEmail());
+
+        return responsavel;
+    }
+}
