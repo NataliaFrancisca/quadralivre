b8d7dec292d84107fc641053309f570ca5af0674
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-14 15:50:40 -0300

    fix: alterei o arquivo de validação que antes estava errado

--- src/main/java/br/com/nat/quadralivre/validation/Estado.java
+++ src/main/java/br/com/nat/quadralivre/validation/Estado.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = DiaSemanaValidacao.class)
+@Constraint(validatedBy = EstadoValidacao.class)
 @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Estado {
