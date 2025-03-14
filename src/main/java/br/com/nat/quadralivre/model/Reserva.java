89d827eb4d6ebe1ac7714f450cf2bcae6aebe437
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-13 23:42:06 -0300

    feat: criei os arquivos necessários para a entidade Reserva

---
+++ src/main/java/br/com/nat/quadralivre/model/Reserva.java
@@ -0,0 +1,45 @@
+package br.com.nat.quadralivre.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "reserva")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long quadraId;
+
+    @Column(nullable = false)
+    private String responsavelCPF;
+
+    @Column(nullable = false)
+    private LocalDateTime data;
+
+    @Column(nullable = false)
+    private LocalTime horarioInicio;
+
+    @Column(nullable = false)
+    private LocalTime horarioEncerramento;
+
+    @ManyToOne
+    @JoinColumn(name = "responsavelCPF", nullable = false, referencedColumnName = "cpf", insertable = false, updatable = false)
+    private Responsavel responsavel;
+
+    @ManyToOne
+    @JoinColumn(name = "quadraId", nullable = false, referencedColumnName = "id", insertable = false, updatable = false)
+    private Quadra quadra;
+}
