7739c93d093501b389fbc7b7248a0824fa6c22ed
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-14 21:12:49 -0300

    style: removi importação que não estava sendo utilizada.

--- src/main/java/br/com/nat/quadralivre/repository/QuadraRepository.java
+++ src/main/java/br/com/nat/quadralivre/repository/QuadraRepository.java
@@ -5,7 +5,6 @@ import br.com.nat.quadralivre.model.Quadra;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface QuadraRepository extends JpaRepository<Quadra, Long> {
     boolean existsByEndereco(Endereco endereco);
