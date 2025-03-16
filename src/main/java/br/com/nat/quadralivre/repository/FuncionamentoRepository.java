31ec3b6711f8576b8a18b950aeb900aec417967a
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-16 20:52:33 -0300

    feat: adicionei novas queries para buscar pelo funcionamento da quadra de acordo com o dia da semana, e outra query que traz o funcionamento ordenado pelos dias da semana.

--- src/main/java/br/com/nat/quadralivre/repository/FuncionamentoRepository.java
+++ src/main/java/br/com/nat/quadralivre/repository/FuncionamentoRepository.java
@@ -1,10 +1,31 @@
 package br.com.nat.quadralivre.repository;
 
+import br.com.nat.quadralivre.enums.DiaSemana;
 import br.com.nat.quadralivre.model.Funcionamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FuncionamentoRepository extends JpaRepository<Funcionamento, Long> {
+    Optional<Funcionamento> findAllByQuadraIdAndDiaSemana(Long quadraId, DiaSemana diaSemana);
+
     List<Funcionamento> findAllByQuadraId(Long quadraId);
+    @Query(value = """
+           SELECT f
+           FROM Funcionamento f
+           WHERE f.quadra_id = :quadraId
+           ORDER BY
+                CASE
+                    WHEN f.diaSemana = 'SEGUNDA' THEN 1
+                    WHEN f.diaSemana = 'TERCA' THEN 2
+                    WHEN f.diaSemana = 'QUARTA' THEN 3
+                    WHEN f.diaSemana = 'QUINTA' THEN 4
+                    WHEN f.diaSemana = 'SEXTA' THEN 5
+                    WHEN f.diaSemana = 'SABADO' THEN 6
+                    WHEN f.diaSemana = 'DOMINGO' THEN 7
+                END
+           """)
+    List<Funcionamento> findAllByQuadraIdOrderByDiaSemana(Long quadraId);
 }
