d3a978f8a841cae85ddf4ba70252ff4d7daa0a6b
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-16 20:50:51 -0300

    refactor: alterei todos os métodos do controller para uma melhor organização e agora uso o RequestParam ao inves de usar o PathVariable

--- src/main/java/br/com/nat/quadralivre/controller/FuncionamentoController.java
+++ src/main/java/br/com/nat/quadralivre/controller/FuncionamentoController.java
@@ -3,6 +3,7 @@ package br.com.nat.quadralivre.controller;
 import br.com.nat.quadralivre.dto.FuncionamentoDTO;
 import br.com.nat.quadralivre.infra.RespostaAPI;
 import br.com.nat.quadralivre.service.FuncionamentoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,47 +21,44 @@ public class FuncionamentoController {
         this.funcionamentoService = funcionamentoService;
     }
 
-    @PostMapping("/{id}")
-    public ResponseEntity<RespostaAPI> create(@PathVariable Long id, @RequestBody List<FuncionamentoDTO> funcionamentoDTOS){
-        this.funcionamentoService.create(id, funcionamentoDTOS);
-
+    @PostMapping()
+    public ResponseEntity<RespostaAPI> create(@RequestParam Long quadraId, @RequestBody List<FuncionamentoDTO> funcionamentoDTOS){
         return RespostaAPI.build(
                 HttpStatus.CREATED,
-                "Dias da semana cadastrada com sucesso."
+                this.funcionamentoService.create(quadraId, funcionamentoDTOS)
         );
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<RespostaAPI> getAll(@PathVariable Long id){
+    @GetMapping
+    public ResponseEntity<RespostaAPI> get(@RequestParam Long quadraId){
         return RespostaAPI.build(
                 HttpStatus.OK,
-                this.funcionamentoService.getAll(id)
+                this.funcionamentoService.get(quadraId)
         );
     }
 
     @PutMapping()
-    public ResponseEntity<RespostaAPI> update(@RequestBody FuncionamentoDTO funcionamentoDTO){
+    public ResponseEntity<RespostaAPI> update(@Valid @RequestBody FuncionamentoDTO funcionamentoDTO){
         return RespostaAPI.build(
                 HttpStatus.OK,
                 this.funcionamentoService.update(funcionamentoDTO)
         );
     }
 
-    @PatchMapping("/{id}/atualizar-disponibilidade")
-    public ResponseEntity<RespostaAPI> updateDisponibilidade(@PathVariable Long id, @RequestParam boolean disponibilidade){
+    @PatchMapping
+    public ResponseEntity<RespostaAPI> updateDisponibilidade(@RequestParam Long quadraId, @RequestParam String diaSemana){
         return RespostaAPI.build(
                 HttpStatus.OK,
-                disponibilidade ? "Acesso à reservas liberadas com sucesso" : "Acesso à reservas bloqueadas com sucesso.",
-                this.funcionamentoService.updateDisponibilidade(id, disponibilidade)
+                this.funcionamentoService.updateDisponibilidade(quadraId, diaSemana)
         );
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<RespostaAPI> delete(@PathVariable Long id){
-        this.funcionamentoService.delete(id);
+    @DeleteMapping
+    public ResponseEntity<RespostaAPI> delete(@RequestParam Long quadraId, @RequestParam String diaSemana){
+        this.funcionamentoService.delete(quadraId, diaSemana);
         return RespostaAPI.build(
                 HttpStatus.OK,
-                "Dia da semana deletado com sucesso."
+                "Funcionamento para o dia selecionado foi deletado com sucesso."
         );
     }
 }
