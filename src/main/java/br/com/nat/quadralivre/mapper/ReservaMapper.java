02f0f848ac66a29614958ea10772fd6c2d6455d0
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-17 23:50:12 -0300

    feat: user o mapper da entidade Quadra

--- src/main/java/br/com/nat/quadralivre/mapper/ReservaMapper.java
+++ src/main/java/br/com/nat/quadralivre/mapper/ReservaMapper.java
@@ -1,14 +1,21 @@
 package br.com.nat.quadralivre.mapper;
 
-import br.com.nat.quadralivre.dto.QuadraDTO;
 import br.com.nat.quadralivre.dto.ReservaCompletaDTO;
 import br.com.nat.quadralivre.dto.ReservaSimplesDTO;
 import br.com.nat.quadralivre.dto.ResponsavelDTO;
 import br.com.nat.quadralivre.model.Reserva;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReservaMapper {
+    private final QuadraMapper quadraMapper;
+
+    @Autowired
+    public ReservaMapper(QuadraMapper quadraMapper) {
+        this.quadraMapper = quadraMapper;
+    }
+
     public ReservaCompletaDTO toDTOCompleto(Reserva reserva){
         return new ReservaCompletaDTO(
                 reserva.getId(),
@@ -16,7 +23,7 @@ public class ReservaMapper {
                 reserva.getHorarioInicio(),
                 reserva.getHorarioEncerramento(),
                 ResponsavelDTO.fromEntity(reserva.getResponsavel()),
-                QuadraDTO.fromEntityShort(reserva.getQuadra())
+                this.quadraMapper.toDTOSimples(reserva.getQuadra())
         );
     }
 
