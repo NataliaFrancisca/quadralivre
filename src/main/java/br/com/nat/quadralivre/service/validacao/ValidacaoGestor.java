2778a4714ef0f0e3b653c40e9c9b324919357e65
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-24 22:03:35 -0300

    feat: criei uma validação para e-mail, assim consigo validar se o RequestParam está recebendo um e-mail válido.

--- src/main/java/br/com/nat/quadralivre/service/validacao/ValidacaoGestor.java
+++ src/main/java/br/com/nat/quadralivre/service/validacao/ValidacaoGestor.java
@@ -1,10 +1,17 @@
 package br.com.nat.quadralivre.service.validacao;
 
+import br.com.caelum.stella.ValidationMessage;
+import br.com.caelum.stella.validation.CPFValidator;
+import br.com.nat.quadralivre.dto.GestorDTO;
+import br.com.nat.quadralivre.exceptions.CPFInvalidoException;
 import br.com.nat.quadralivre.model.Gestor;
 import br.com.nat.quadralivre.repository.GestorRepository;
+import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
 
 @Service
 public class ValidacaoGestor {
@@ -29,18 +36,27 @@ public class ValidacaoGestor {
         this.validarUnicidade("telefone", telefone, this.gestorRepository.existsByTelefone(telefone));
     }
 
-    public void validar(Gestor gestor){
+    public void validar(GestorDTO gestor){
         this.verificarEmailExistente(gestor.getEmail());
         this.verificarTelefoneExistente(gestor.getTelefone());
     }
 
-    public void validarAtualizacao(Gestor gestor, Gestor gestorAtualizado){
+    public void validarAtualizacao(Gestor gestor, GestorDTO gestorAtualizado){
         if(!gestor.getEmail().equals(gestorAtualizado.getEmail())){
             this.verificarEmailExistente(gestorAtualizado.getEmail());
         }
 
         if(!gestor.getTelefone().equals(gestorAtualizado.getTelefone())){
             this.verificarTelefoneExistente(gestorAtualizado.getTelefone());
+        }
+    }
+
+    public void validarEmail(String email){
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+
+        if(!pattern.matcher(email).matches()){
+            throw new IllegalArgumentException("Digite um e-mail válido. Ex.: email@email.com");
         }
     }
 }
