526a7f6f359bb50b23b1fb7515db374211be750c
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-26 00:02:21 -0300

    refactor: refiz as validações porque na verdade a regra não estava bem definida.

--- src/main/java/br/com/nat/quadralivre/service/validacao/ValidacaoQuadra.java
+++ src/main/java/br/com/nat/quadralivre/service/validacao/ValidacaoQuadra.java
@@ -4,6 +4,7 @@ import br.com.nat.quadralivre.dto.EnderecoDTO;
 import br.com.nat.quadralivre.dto.QuadraDTO;
 import br.com.nat.quadralivre.model.Quadra;
 import br.com.nat.quadralivre.repository.QuadraRepository;
+import br.com.nat.quadralivre.util.EmailUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -11,31 +12,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class ValidacaoQuadra {
     final private QuadraRepository quadraRepository;
+    final private EmailUtils emailUtils;
 
     @Autowired
-    public ValidacaoQuadra(QuadraRepository quadraRepository){
+    public ValidacaoQuadra(QuadraRepository quadraRepository, EmailUtils emailUtils){
         this.quadraRepository = quadraRepository;
+        this.emailUtils = emailUtils;
     }
 
-    private void verificarTituloExistente(String titulo){
-        if(this.quadraRepository.existsByTitulo(titulo)){
-            throw new DataIntegrityViolationException("Já existe uma quadra com esse título");
-        }
-    }
+    public void validar(QuadraDTO quadraDTO){
+        boolean quadraComTituloJaCadastradaNoEndereco = this.quadraRepository
+                .existsByTituloAndEndereco(quadraDTO.getTitulo(), EnderecoDTO.toEndereco(quadraDTO.getEndereco()));
 
-    public void validar(QuadraDTO quadra){
-        boolean tituloExiste = this.quadraRepository.existsByTitulo(quadra.getTitulo());
-        boolean enderecoExiste = this.quadraRepository.existsByEndereco(EnderecoDTO.toEndereco(quadra.getEndereco()));
-
-        if(tituloExiste && enderecoExiste){
+        if(quadraComTituloJaCadastradaNoEndereco) {
             throw new DataIntegrityViolationException("Já existe um cadastro para essa quadra nesse endereço.");
         }
     }
 
     public void validarAtualizacao(Quadra quadra, QuadraDTO quadraAtualizada){
         if(!quadra.getTitulo().equals(quadraAtualizada.getTitulo())){
-            this.verificarTituloExistente(quadraAtualizada.getTitulo());
+            this.validar(quadraAtualizada);
         }
+    }
+
+    public void validarEmail(String email){
+        this.emailUtils.validarEmail(email);
     }
 }
 
