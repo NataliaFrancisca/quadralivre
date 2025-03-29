6d73dc42fd97cdfefc3a3edf7c7d0e5e9fb80ddf
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-28 21:41:58 -0300

    feat: adicionei o mapper do responsavel e criei um método para criar uma entidade Reserva.

--- src/main/java/br/com/nat/quadralivre/mapper/ReservaMapper.java
+++ src/main/java/br/com/nat/quadralivre/mapper/ReservaMapper.java
@@ -1,8 +1,10 @@
 package br.com.nat.quadralivre.mapper;
 
 import br.com.nat.quadralivre.dto.ReservaCompletaDTO;
+import br.com.nat.quadralivre.dto.ReservaDTO;
 import br.com.nat.quadralivre.dto.ReservaSimplesDTO;
 import br.com.nat.quadralivre.dto.ResponsavelDTO;
+import br.com.nat.quadralivre.model.HorarioDisponivel;
 import br.com.nat.quadralivre.model.Reserva;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,10 +12,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class ReservaMapper {
     private final QuadraMapper quadraMapper;
+    private final ResponsavelMapper responsavelMapper;
 
     @Autowired
-    public ReservaMapper(QuadraMapper quadraMapper) {
+    public ReservaMapper(QuadraMapper quadraMapper, ResponsavelMapper responsavelMapper) {
         this.quadraMapper = quadraMapper;
+        this.responsavelMapper = responsavelMapper;
     }
 
     public ReservaCompletaDTO toDTOCompleto(Reserva reserva){
@@ -22,7 +26,7 @@ public class ReservaMapper {
                 reserva.getData(),
                 reserva.getHorarioInicio(),
                 reserva.getHorarioEncerramento(),
-                ResponsavelDTO.fromEntity(reserva.getResponsavel()),
+                this.responsavelMapper.toDTOCompleto(reserva.getResponsavel()),
                 this.quadraMapper.toDTOSimples(reserva.getQuadra())
         );
     }
@@ -33,7 +37,19 @@ public class ReservaMapper {
                 reserva.getData(),
                 reserva.getHorarioInicio(),
                 reserva.getHorarioEncerramento(),
-                ResponsavelDTO.fromEntity(reserva.getResponsavel())
+                this.responsavelMapper.toDTOSimples(reserva.getResponsavel())
         );
+    }
+
+    public Reserva toReservaEntidade(ReservaDTO reservaDTO, HorarioDisponivel horarioReserva){
+        Reserva reserva = new Reserva();
+
+        reserva.setQuadraId(reservaDTO.getQuadraId());
+        reserva.setResponsavelCPF(reservaDTO.getResponsavelCPF());
+        reserva.setData(reservaDTO.getDataSolicitada().atTime(horarioReserva.getHorarioInicio()));
+        reserva.setHorarioInicio(horarioReserva.getHorarioInicio());
+        reserva.setHorarioEncerramento(horarioReserva.getHorarioEncerramento());
+
+        return reserva;
     }
 }
