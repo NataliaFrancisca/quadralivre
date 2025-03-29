0396290879095cfea182ff9ef4c1e0be77802253
nataliafrancisca <nathifrancisca@gmail.com>
2025-03-28 21:21:45 -0300

    feat: criei um mapper para colocar os métodos que estavam na classe DTO

--- src/main/java/br/com/nat/quadralivre/service/finder/ReservaFinderService.java
+++ src/main/java/br/com/nat/quadralivre/service/finder/ReservaFinderService.java
@@ -6,29 +6,20 @@ import br.com.nat.quadralivre.model.Quadra;
 import br.com.nat.quadralivre.model.Responsavel;
 import br.com.nat.quadralivre.repository.QuadraRepository;
 import br.com.nat.quadralivre.repository.ResponsavelRepository;
-import br.com.nat.quadralivre.service.HorarioDisponivelService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ReservaFinderService {
     private final ResponsavelRepository responsavelRepository;
     private final QuadraRepository quadraRepository;
-    private final HorarioDisponivelService horarioDisponivelService;
+    private final HorarioDisponivelFinderService horarioDisponivelFinderService;
 
     public HorarioDisponivel buscarHorarioReserva(ReservaDTO reserva){
-        Map<Integer, HorarioDisponivel> horariosDisponiveis = this.horarioDisponivelService.buscarHorariosDisponiveis(
-                reserva.getQuadraId(),
-                reserva.getDataSolicitada()
-        );
-
-        return Optional.ofNullable(horariosDisponiveis.get(reserva.getHorarioDisponivelId())).orElseThrow(() ->
-                new EntityNotFoundException("Horário de reserva com ID " + reserva.getHorarioDisponivelId() + " não encontrado."));
+        return horarioDisponivelFinderService.buscarHorarioReserva(reserva);
     }
 
     public Quadra buscarQuadra(Long quadraId){
